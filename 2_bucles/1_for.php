d58f20a87f129e76a65ac1061873f2b8d4c42446
rodri-afa <rodrigo.afaformacion@gmail.com>
2022-09-28 14:27:58 +0200

    for ul li

--- 2_bucles/1_for.php
+++ 2_bucles/1_for.php
@@ -9,3 +9,9 @@ for ($i=1; $i <= 10; $i++) {
 //Ej: 4 x 1 = 4
 //Ej: 4 x 2 = 8
 //Ej: 4 x 3 = 12
+
+echo("<br>");
+define("TABLA", 2);
+for ($i=0; $i <= 10 ; $i++) { 
+    echo(TABLA . " x " . $i ."=". TABLA* $i. "<br>");
+}
---
+++ 2_bucles/2_forTablas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tablas de multiplicar</title>
+</head>
+<body>
+    <ul>
+        <?php
+            define("TABLA", 2);
+            for ($i=0; $i <= 10 ; $i++) { 
+            echo("<li>".TABLA . " x " . $i ."=". TABLA* $i . "</li>");
+            }
+        ?> 
+    </ul>
+    
+</body>
+</html>
+
+
