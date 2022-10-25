1aa6f1471447591cd765675cb1de3704d424b17e
rodri-afa <rodrigo.afaformacion@gmail.com>
2022-10-25 10:39:28 +0200

    enunciado triangulo

---
+++ 5_clases/2_triangulo.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Clase triángulo
+ * propiedades:base, altura, área.
+ * Métodos:
+ *  -construct
+ *  -setters
+ *  -getters
+ *  -toString:
+ *      Esto es un objeto de la clase ...
+ *      Base: x
+ *      Altura: x
+ *      Área: x 
+ * 
+ */
