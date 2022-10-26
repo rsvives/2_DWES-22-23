0894c95e58b99086da739bccc40c1df25a62b5d0
rodri-afa <rodrigo.afaformacion@gmail.com>
2022-10-26 14:29:46 +0200

    herencia

---
+++ 5_clases/3_persona.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Clase persona
+ * Propiedades:
+ *  - nombre
+ *  - altura
+ *  - peso
+ *  - patrimonio
+ * 
+ * Métodos:
+ *  -construct
+ *  -getters y setters
+ *  -toString
+ *  -saludar -> necesita el nombre de otra persona, echo "Hola menganita"
+ *              si no le paso ningún nombre, echo "Hola desconocid@"          
+ */
+
+/**
+ * Clase trabajador
+ * Propiedades:
+ *  -Sueldo
+ *  -Empresa
+ * 
+ * Métodos:
+ *  -construct
+ *  -getters y setters
+ *  -toString
+ *  -Cobrar
+ * 
+ */
