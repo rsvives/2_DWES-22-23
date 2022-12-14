d9473e53a5172b02547cf477b424914849437852
rodri-afa <rodrigo.afaformacion@gmail.com>
2022-12-14 13:48:46 +0100

    title fix

--- 7_cookies_y_sesiones/2_idioma/index.php
+++ 7_cookies_y_sesiones/2_idioma/index.php
@@ -28,7 +28,7 @@ if (!empty($_COOKIE['idioma'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tizziano Sapo</title>
+    <title>Inicio</title>
 </head>
 
 <body>
