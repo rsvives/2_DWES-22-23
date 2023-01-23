7a8ed21d64d916a98d147ee2e7e821402ea5f28a
rodri-afa <rodrigo.afaformacion@gmail.com>
2023-01-23 10:29:38 +0100

    transacciones

---
+++ 8_bbdd/6_transacciones.php
@@ -0,0 +1,28 @@
+<?php
+
+// Ejemplo excepcion
+// try{
+//     $algo = "cosa";
+// }catch(Exception $e){
+//    echo $e->getMessage();
+// }
+
+
+
+$bd = new PDO("mysql:host=localhost:3307;dbname=dwes", "root", "");
+$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try {
+    $bd->beginTransaction();
+    // insert
+    // insert
+    // insert
+    $bd->commit();
+} catch (Exception $e) {
+    try {
+        echo "Error haciendo rollback... error:" . $e->getMessage();
+        $bd->rollBack();
+    } catch (Exception $e2) {
+        echo "El rollback ha fallado. Error: " . $e2->getMessage();
+    }
+}
