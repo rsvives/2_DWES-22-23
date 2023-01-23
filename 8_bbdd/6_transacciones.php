30a239129d109a94034faf17a657364feae94724
rodri-afa <rodrigo.afaformacion@gmail.com>
2023-01-23 12:08:31 +0100

    transacciones

--- 8_bbdd/6_transacciones.php
+++ 8_bbdd/6_transacciones.php
@@ -11,12 +11,13 @@
 
 $bd = new PDO("mysql:host=localhost:3307;dbname=dwes", "root", "");
 $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 try {
     $bd->beginTransaction();
-    // insert
-    // insert
-    // insert
+    $insert = $bd->exec("INSERT INTO usuarios(nombre,email,passwrd,direccion,tarjeta_credito)
+    VALUES ('hugo','hugo@mail.com','hffewofu2','direccion_hugo','123746323')");
+    $insert2 = $bd->exec("INSERT INTO usuarios(nombre,email,passwrd,direccion,tarjeta_credito)
+    VALUES('siro','siro@mail.com','jfwoeif234','direccion_siro','854386546')");
+    $insert3 = $bd->exec("INSERT INTO usuarios(n,email,passwrd,direccion,tarjeta_credito) VALUES('pedro','pedrojodete@mail.com','geirg243','direccion_pedro','436457647')");
     $bd->commit();
 } catch (Exception $e) {
     try {
